d595cd309132cb51b0f20f46525cda02b833553b
only <hwy113329@163.com>
2022-01-24 17:35:21 +0800

    删除倒数第N个结点

--- src/leetCodeHot100/list/RemoveNthFromEnd.java
+++ src/leetCodeHot100/list/RemoveNthFromEnd.java
@@ -1,6 +1,7 @@
 package leetCodeHot100.list;
 
 
+import java.util.List;
 
 public class RemoveNthFromEnd {
     public ListNode removeNthFromEnd(ListNode head, int n) {
@@ -8,18 +9,51 @@ public class RemoveNthFromEnd {
         /*
         * 思路：先让第一个指针指向正数的第N
         * */
-        ListNode cur = head;
-        for (int i = 1; i < n; i++) {
-            cur = cur.next;
+//        ListNode cur = head;
+//        for (int i = 1; i < n; i++) {
+//            cur = cur.next;
+//        }
+//        ListNode deleteNode = head;
+//        int i = 0;
+//        while (cur != null) {
+//            if(i>=2) deleteNode = deleteNode.next;
+//            cur = cur.next;
+//            i++;
+//        }
+//        deleteNode.next = deleteNode.next.next;
+//        return head;
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode start = pre;
+        ListNode end = head;
+        int i = n;
+        while (i != 0) {
+            start = start.next;
+            i--;
         }
-        ListNode deleteNode = head;
-        int i = 0;
-        while (cur != null) {
-            if(i>=2) deleteNode = deleteNode.next;
-            cur = cur.next;
-            i++;
+
+        while (start.next != null) {
+            start = start.next;
+            end = end.next;
         }
-        deleteNode.next = deleteNode.next.next;
-        return head;
+
+        end.next = end.next.next;
+        return pre.next;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
