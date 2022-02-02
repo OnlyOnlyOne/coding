71f06770072084b75741519c2791bfa0009e4fe7
only <hwy113329@163.com>
2022-02-02 22:55:13 +0800

    二分：左闭右闭

---
+++ src/array/Lc704Search.java
@@ -0,0 +1,42 @@
+package array;
+
+public class Lc704Search {
+//    public int search(int[] nums, int target) {
+//        //使用二分查找
+//        //方法一：左闭右闭空间
+//        int length = nums.length;
+//        return erfenSearch(nums, 0, length - 1, target);
+//
+//    }
+//
+//    private int erfenSearch(int[] nums, int left, int right, int target) {
+//        if(left<=right) return -1;
+//        int length = nums.length;
+//        if(nums[(right - left)/2] ==target ) return (right - left)/2;
+//        int a = erfenSearch(nums,left,length/2,target);
+//        int b = erfenSearch(nums,length/2+1,right,target);
+//        if(a == target ) return a;
+//        else if (b == target) { return b;
+//        } else return -1;
+//    }
+
+    public int search(int[] nums, int target) {
+        //使用二分查找
+        //方法一：左闭右闭空间
+        // 避免当 target 小于nums[0] nums[nums.length - 1]时多次循环运算
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else if (nums[mid] > target)
+                right = mid - 1;
+        }
+        return -1;
+    }
+}
