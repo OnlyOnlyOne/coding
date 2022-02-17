a8ebdc55ee1f6978d5ee4cdbdeee472348a55189
only <hwy113329@163.com>
2022-02-17 16:33:37 +0800

    lc344

---
+++ src/string/Lc344翻转字符串.java
@@ -0,0 +1,20 @@
+package string;
+
+public class Lc344翻转字符串 {
+    public void reverseString(char[] s) {
+        if(s.length == 0) return;
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            swap(left, right, s);
+            left++;
+            right--;
+        }
+    }
+
+    public void swap(int left, int right,char[] s) {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+    }
+}
